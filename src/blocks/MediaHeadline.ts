b7edcde22782260accaead008733451495e84d42
Josneth Moreno <josnethmoreno@gmail.com>
2023-10-12 17:58:09 -0400

    Remove required in Media Headline Block

--- src/blocks/MediaHeadline.ts
+++ src/blocks/MediaHeadline.ts
@@ -12,7 +12,6 @@ const MediaHeadline: Block = {
       label: 'media',
       type: 'upload',
       relationTo: 'media',
-      required: true,
       admin: {
         description: 'Maximum upload file size: 12MB. Recommended file size for images is <500KB.',
       },
